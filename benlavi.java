9f96ed3214102fa56030719b6f33a27a318f4b2c
Ben Lavi <87126461+Benlavi@users.noreply.github.com>
2025-11-18 16:06:48 +0200

    added My name is .. ... + i

--- benlavi.java
+++ benlavi.java
@@ -1,7 +1,7 @@
 public class benlavi {
     public static void main (String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Ben Lavi");
+            System.out.println("My Name is Ben Lavi " + i);
         }
     }
 }
